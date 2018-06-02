52d251ebd4f3030d5207752453bd8e5778f194b4
Andrew Lewis <andrewlewis@andrews-air-3.home>
2018-06-02 23:17:08 +0100

    Basic footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,25 @@
+<template>
+<div class="wrapper">
+
+  <div class="module-footer">
+    <div class="inner">
+     <p> Made by Andrew and Mike</p>
+    </div>
+  </div>
+
+</div>
+</template>
+
+<style lang="scss" scoped>
+@import '../style';
+
+.module-footer{
+  background: $Background;
+  @include layout-frame-inner($Frame-Width);
+  .inner{
+      background: green;
+
+  }
+}
+
+</style>
