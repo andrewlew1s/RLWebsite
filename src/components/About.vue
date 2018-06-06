aa599b39bed042a2ce1eeab31201639f706fbc71
Andrew Lewis <andrewlewis@andrews-air-3.home>
2018-06-07 00:20:48 +0100

    Added about functionality

---
+++ src/components/About.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="module-about">
+        <div class="inner">
+            <b-img src="../assets/rachel.jpg" class="rachel"></b-img>
+            <p> Rachel Lewis is a 2014 winner of the Cape Farewell/Young Poets Network competition for poems exploring climate change.</p>
+        </div>
+    </div>
+</template>
+
+<script>
+</script>
+
+<style lang="scss" scoped>
+@import '../style';
+
+.module-about{
+    $height: 100px;
+    background: $Highlight;
+  @include layout-frame-inner();
+  .rachel{
+        height: $height / 2;
+		margin: auto;
+        margin-top: $height / 10;
+        display: block;
+  }
+  .inner{
+  }
+}
+
+</style>
