7a1399b3279a4e02562768be8f1cfcf4d1946634
Michael <michael.fitzhavey@gmail.com>
2018-06-09 16:31:08 +0100

    -

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
 import Vuex from 'vuex';
-import * as firebase from 'firebase';
+import firebase from 'firebase';
 
 import BootstrapVue from 'bootstrap-vue';
 import 'bootstrap/dist/css/bootstrap.css';
