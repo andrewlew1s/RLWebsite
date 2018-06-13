a3aff5945c798ce7b82fe87eb555b89b8d0a12c1
Michael <michael.fitzhavey@gmail.com>
2018-06-13 20:21:29 +0100

    added firebase to app and wired up poems into router-view

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,52 @@
 <template>
   <div id="app">
     <Header></Header>
-    <router-view class="wrapper"/>
+    <router-view class="wrapper"
+		:poems="poems"/>
     <Footer></Footer>
   </div>
 </template>
 
 <script>
+import Firebase from 'firebase';
 import Header from './components/Header';
 import Footer from './components/Footer';
+
+const firebaseConfig = {
+	apiKey: 'AIzaSyCBSeq7wRNRQQYZnxZGYt35P-6-yHvwXIg',
+	authDomain: 'asdsa-48c2f.firebaseapp.com',
+	databaseURL: 'https://asdsa-48c2f.firebaseio.com',
+	projectId: 'asdsa-48c2f',
+	storageBucket: 'asdsa-48c2f.appspot.com',
+	messagingSenderId: '92593551317'
+};
+
+const app = Firebase.initializeApp(firebaseConfig);
+const db = app.database();
+const poemsRef = db.ref('poems');
 
 export default {
 	name: 'App',
 	components: {
 		Header,
 		Footer
+	},
+	data() {
+		return {
+			loadingPoems: false,
+			poems: []
+		};
+	},
+	methods: {
+		async loadPoems() {
+			this.loadingPoems = true;
+			const snap = await poemsRef.once('value');
+			this.poems = snap.val();
+			this.loadingPoems = false;
+		}
+	},
+	mounted() {
+		this.loadPoems();
 	}
 };
 </script>
