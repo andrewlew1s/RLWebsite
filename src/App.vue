301bcbfd980626dfcecf9effc46fda66d95b63b8
Michael <michael.fitzhavey@gmail.com>
2018-07-14 13:06:49 +0100

    remove old poems logic

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,7 @@
 
 		<Header/>
 
-		<router-view
-			class="App__wrapper"
-			:poems="poems"/>
+		<router-view class="App__wrapper"/>
 
 		<Footer/>
 
@@ -13,53 +11,14 @@
 </template>
 
 <script>
-import Firebase from 'firebase';
 import Header from './components/Header';
 import Footer from './components/Footer';
-
-const firebaseConfig = {
-	apiKey: 'AIzaSyCjOdEJb-yC05RldJ_orSg8URsW42jWaA8',
-	authDomain: 'demoworld-c9c3a.firebaseapp.com',
-	databaseURL: 'https://demoworld-c9c3a.firebaseio.com',
-	projectId: 'demoworld-c9c3a',
-	storageBucket: 'demoworld-c9c3a.appspot.com',
-	messagingSenderId: '994402680084'
-};
-
-const app = Firebase.initializeApp(firebaseConfig);
-const db = app.database();
-const poemsRef = db.ref('poems');
 
 export default {
 	name: 'App',
 	components: {
 		Header,
 		Footer
-	},
-	data() {
-		return {
-			loadingPoems: false,
-			poems: []
-		};
-	},
-	methods: {
-		async loadPoems() {
-			this.loadingPoems = true;
-			const snap = await poemsRef.once('value');
-			this.poems = this.poemIdFactory(snap.val());
-			this.loadingPoems = false;
-		},
-		// When given an array of poems, simply returns the same array but with the
-		// property 'id' added to each poem and the value of its position in the array
-		poemIdFactory(poems) {
-			return poems.map((poem, i) => {
-				poem.id = i;
-				return poem;
-			});
-		}
-	},
-	mounted() {
-		this.loadPoems();
 	}
 };
 </script>
