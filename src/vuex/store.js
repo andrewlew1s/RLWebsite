5ab9faca589029a80872e7154ba796478b5dc2bc
Michael <michael.fitzhavey@gmail.com>
2018-07-25 04:54:23 +0100

    updated store

---
+++ src/vuex/store.js
@@ -0,0 +1,27 @@
+import firebase from 'firebase';
+import poemStore from './modules/poemStore';
+
+
+const firebaseConfig = {
+	apiKey: 'AIzaSyCjOdEJb-yC05RldJ_orSg8URsW42jWaA8',
+	authDomain: 'demoworld-c9c3a.firebaseapp.com',
+	databaseURL: 'https://demoworld-c9c3a.firebaseio.com',
+	projectId: 'demoworld-c9c3a',
+	storageBucket: 'demoworld-c9c3a.appspot.com',
+	messagingSenderId: '994402680084'
+};
+
+const app = firebase.initializeApp(firebaseConfig);
+const db = app.database();
+const dataRef = db.ref();
+
+const store = {
+	state: {
+		data: dataRef
+	},
+	modules: {
+		poem: poemStore
+	}
+};
+
+export default store;
