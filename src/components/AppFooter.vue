b28d5d1f1c6892d57c670445986ab053eefbde23
Michael <michael.fitzhavey@gmail.com>
2018-07-27 19:30:47 +0100

    remove font icons

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -2,12 +2,6 @@
 	<div class="Footer">
 		<div class="App__inner Footer__inner">
 			<span> Andrew and Mike Ltd. Corp</span>
-			<section class="Footer__icons">
-				<font-awesome-icon icon="bong"/>
-				<img
-					class="Footer__icon"
-					src="/static/icons/facebook.svg"/>
-			</section>
 		</div>s
 	</div>
 </template>
--- src/main.js
+++ src/main.js
@@ -5,18 +5,11 @@ import Vuex from 'vuex';
 import BootstrapVue from 'bootstrap-vue';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faBong } from '@fortawesome/free-solid-svg-icons';
 
 import storeConfig from './vuex/store';
 import App from './App';
 import router from './router';
 import AppLoader from './components/AppLoader';
-
-library.add(faBong);
-Vue.component('font-awesome-icon', FontAwesomeIcon);
-
 
 Vue.config.productionTip = false;
 
