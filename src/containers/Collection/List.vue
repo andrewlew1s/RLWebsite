989abf62d7cfb83cc42c73a1ec8f1ee7a2d9f784
michaelfitzhavey <michael.fitzhavey@gmail.com>
2018-07-29 11:00:47 +0100

    use local images

--- src/containers/Collection/List.vue
+++ src/containers/Collection/List.vue
@@ -5,7 +5,7 @@
 			<b-card
 				class="Poems__card"
 				overlay
-				img-src="https://images.unsplash.com/photo-1513545581681-9217042d29b8?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6c95f0ea2768c222f671086d5ba6640d&auto=format&fit=crop&w=1350&q=80"
+				img-src="/static/images/published.jpg"
 				img-alt="Published"
 				text-variant="white"
 				title="Published">
@@ -16,7 +16,7 @@
 			<b-card
 				class="Poems__card"
 				overlay
-				img-src="https://images.unsplash.com/photo-1507676184212-d03ab07a01bf?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=3a413a0b054159dd7840130c25e6dbdf&auto=format&fit=crop&w=1350&q=80"
+				img-src="/static/images/performed.jpg"
 				img-alt="Perfomed"
 				text-variant="white"
 				title="Perfomed">
