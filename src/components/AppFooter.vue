22473b1a756ebc65c5037167ca9fff16deb8d42a
michaelfitzhavey <michael.fitzhavey@gmail.com>
2018-08-23 11:35:10 +0100

    feat: style icons and add hover states

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,24 +1,29 @@
 <template>
-	<div class="Footer">
+	<section class="Footer">
 		<div class="App__inner Footer__inner">
-			<b-col>
 
-				<b-link href="https://twitter.com/Rachel_Claire_L">
-					<b-img
-						class="Footer__icon"
-						src="/static/images/tweet.JPG"/>
-				</b-link>
-
-				<b-link href="https://www.facebook.com/profile.php?id=1186786120">
-					<b-img
-						class="Footer__icon Footer__icon--facebook"
-						src="/static/icons/facebook.svg"/>
-				</b-link>
-
-		</b-col>
 			<span> This website is a product of Holistic Web Studios</span>
+
+			<div class="Footer__icons">
+				<b-link
+					class="Footer__icon"
+					href="https://twitter.com/Rachel_Claire_L"
+					target="_blank"
+					title="Follow on Twitter!">
+					<b-img src="/static/images/tweet.JPG"/>
+				</b-link>
+
+				<b-link
+					class="Footer__icon Footer__icon--facebook"
+					href="https://www.facebook.com/profile.php?id=1186786120"
+					target="_blank"
+					title="View Facebook!">
+					<b-img src="/static/icons/facebook.svg"/>
+				</b-link>
+			</div>
+
 		</div>
-	</div>
+	</section>
 </template>
 
 <script>
@@ -50,7 +55,13 @@ $textColour: $Highlight-Colour;
 	float: left;
 
 	&__inner {
-		padding-top: 0.5em;
+		padding-top: 1.5em;
+	}
+
+	&__row {
+		width: 100%;
+		display: block;
+		margin-bottom: 16px;
 	}
 
 	&__icons {
@@ -58,11 +69,24 @@ $textColour: $Highlight-Colour;
 	}
 
 	&__icon {
-		width: 24px;
 		margin: 0 8px;
+		float: left;
+
+		> img {
+			width: 24px;
+		}
+
+		&:last-of-type {
+			margin-right: 0;
+		}
 
 		&--facebook {
 			background: white;
+
+			> img {
+				width: 26px;
+			}
+
 		}
 	}
 }
